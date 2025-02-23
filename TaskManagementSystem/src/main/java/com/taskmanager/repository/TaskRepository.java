03d40a1c4e4cd810fc1b12182f74d1426c6d65f5
anuyrm <anu.yrm@gmail.com>
2025-02-23 20:15:52 +0000

    Create TaskRepository.java

---
+++ TaskManagementSystem/src/main/java/com/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.taskmanager.repository;
+
+import com.taskmanager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(String status);
+}
